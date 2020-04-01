573124f4e1778483fedaa7db329ac73be1a861f7
Inhji <johnnie@posteo.de>
2020-04-01 22:11:13 +0200

    add fake user for testing authenticated routes

--- apps/tomie_web/test/tomie_web/controllers/page_controller_test.exs
+++ apps/tomie_web/test/tomie_web/controllers/page_controller_test.exs
@@ -1,8 +1,16 @@
 defmodule TomieWeb.PageControllerTest do
   use TomieWeb.ConnCase
 
+  # See: https://elixirforum.com/t/controller-test-with-pow/18926/2
+  setup %{conn: conn} do
+    user = %Tomie.Users.User{email: "test@example.com", id: 1}
+    authed_conn = Pow.Plug.assign_current_user(conn, user, [])
+
+    {:ok, conn: authed_conn}
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Tomie"
   end
 end
