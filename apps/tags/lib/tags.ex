5ea635a748df89e47e52b823df29ac9b4f2a69e0
Inhji <johnnie@posteo.de>
2020-04-03 21:09:35 +0200

    add tags app

---
+++ apps/tags/lib/tags.ex
@@ -0,0 +1,18 @@
+defmodule Tags do
+  @moduledoc """
+  Documentation for `Tags`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tags.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/tags/test/tags_test.exs
@@ -0,0 +1,8 @@
+defmodule TagsTest do
+  use ExUnit.Case
+  doctest Tags
+
+  test "greets the world" do
+    assert Tags.hello() == :world
+  end
+end
