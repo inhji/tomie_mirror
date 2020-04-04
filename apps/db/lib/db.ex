06e332310c768fed50919d64ce878a75d0f8b1fe
Inhji <johnnie@posteo.de>
2020-04-04 18:37:28 +0200

    clean up db

--- apps/db/lib/db.ex
+++ apps/db/lib/db.ex
@@ -2,17 +2,4 @@ defmodule Db do
   @moduledoc """
   Documentation for `Db`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Db.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/db/test/db_test.exs
+++ apps/db/test/db_test.exs
@@ -1,8 +1,3 @@
 defmodule DbTest do
   use ExUnit.Case
-  doctest Db
-
-  test "greets the world" do
-    assert Db.hello() == :world
-  end
 end
