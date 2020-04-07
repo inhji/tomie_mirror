7e8f20d9ae0e3507a48b19100e9eb7a8d40479ac
Inhji <johnnie@posteo.de>
2020-04-07 08:33:28 +0200

    tag helpers

--- apps/tomie_web/lib/tomie_web/views/tag_view.ex
+++ apps/tomie_web/lib/tomie_web/views/tag_view.ex
@@ -1,3 +1,22 @@
 defmodule TomieWeb.TagView do
   use TomieWeb, :view
+
+  def rule_count(tag) do
+    case tag.rules do
+      nil ->
+        0
+
+      rules ->
+        rules
+        |> String.split("\n")
+        |> Enum.count()
+    end
+  end
+
+  def rules(tag) do
+    case tag.rules do
+      nil -> []
+      rules -> rules |> String.split("\n")
+    end
+  end
 end
