6fbddb827f1f232607bba91cfce4d745328f06a6
Inhji <johnnie@posteo.de>
2020-04-08 08:41:02 +0200

    add token and theme to user

--- apps/tomie/lib/tomie/users/user.ex
+++ apps/tomie/lib/tomie/users/user.ex
@@ -1,10 +1,32 @@
-defmodule Tomie.Users.User do
+defmodule Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
+  import Ecto.Changeset
 
   schema "users" do
     pow_user_fields()
 
+    field :theme, :string, default: "light"
+    field :token, :string
+    field :reset_token, :boolean, virtual: true
+
     timestamps()
+  end
+
+  def profile_changeset(user, attrs \\ %{}) do
+    user
+    |> cast(attrs, [:theme, :token, :reset_token])
+    |> maybe_reset_token()
+  end
+
+  defp maybe_reset_token(changeset) do
+    case get_change(changeset, :reset_token, false) do
+      true -> put_change(changeset, :token, token(20))
+      false -> changeset
+    end
+  end
+
+  defp token(length) do
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64() |> binary_part(0, length)
   end
 end
