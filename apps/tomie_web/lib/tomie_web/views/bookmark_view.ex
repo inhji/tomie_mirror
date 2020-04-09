3e6c43213593d469d72e0c2d732eb20045e6421f
Inhji <johnnie@posteo.de>
2020-04-09 09:17:03 +0200

    add list style

--- apps/tomie_web/lib/tomie_web/views/bookmark_view.ex
+++ apps/tomie_web/lib/tomie_web/views/bookmark_view.ex
@@ -1,12 +1,3 @@
 defmodule TomieWeb.BookmarkView do
   use TomieWeb, :view
-
-  def tags(%{tags: []}), do: "None"
-  def tags(%{tags: [tag]}), do: tag.name
-
-  def tags(%{tags: tags}),
-    do:
-      tags
-      |> Enum.map(&Map.get(&1, :name))
-      |> Enum.join(",")
 end
