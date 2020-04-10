2cf5452067bf6b096964434c0a50bf092de09177
Inhji <johnnie@posteo.de>
2020-04-10 14:45:04 +0200

    add hover variants for navbar items

--- apps/tomie_web/assets/theme.config.js
+++ apps/tomie_web/assets/theme.config.js
@@ -20,15 +20,22 @@ const mainTheme = new Theme()
     'on-danger': '#C53030'
   })
   .opacityVariant('muted', .8, ['on-surface'])
+  .colorVariant('hover', '#EDF2F7', 'navbar')
+  .colorVariant('hover', '#4C51BF', 'on-navbar')
 
 const darkTheme = new Theme()
   .colors({
     'on-surface': '#CBD5E0',
     'surface': '#2D3748',
 
+    'navbar': '#4C51BF',
+    'on-navbar': '#EDF2F7',
+
     'danger': '#9B2C2C',
     'on-danger': '#FEB2B2'
-  });
+  })
+  .colorVariant('hover', '#2D3748', 'navbar')
+  .colorVariant('hover', '#CBD5E0', 'on-navbar')
 
 module.exports = new ThemeBuilder()
   .asDataThemeAttribute()
