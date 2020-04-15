fccee21470b744ff93eb30d49a7473d419881459
Inhji <johnnie@posteo.de>
2020-04-15 17:15:01 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+mix local.hex --force
+mix local.rebar --force
 mix deps.get --only prod
-MIX_ENV=prod mix compile
+MIX_ENV=prod mix deps.compile
 
 npm install --prefix ./apps/tomie_web/assets
 npm run deploy --prefix ./apps/tomie_web/assets
-mix phx.digest
+MIX_ENV=prod mix phx.digest
 
 rm -rf "_build"
 MIX_ENV=prod mix release
