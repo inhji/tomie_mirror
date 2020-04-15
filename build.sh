aef0f7a12c0bac6b9eb9424d75b955e7e26e0504
Inhji <johnnie@posteo.de>
2020-04-15 17:33:34 +0200

    overwrite release

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ MIX_ENV=prod mix deps.compile
 npm install --prefix ./apps/tomie_web/assets
 npm run deploy --prefix ./apps/tomie_web/assets
 MIX_ENV=prod mix phx.digest
-MIX_ENV=prod mix release
+MIX_ENV=prod mix release --overwrite
