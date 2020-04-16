f5d0995f12987b59d5a382d28d93b3971b199395
Inhji <johnnie@posteo.de>
2020-04-16 08:39:29 +0200

    fix worker not sending updated tags, show tags on bookmark

--- apps/bookmarks/lib/bookmarks/worker.ex
+++ apps/bookmarks/lib/bookmarks/worker.ex
@@ -5,9 +5,11 @@ defmodule Bookmarks.Worker do
     with {:ok, html} <- Scraper.get_html(source),
          {:ok, result} <- Scraper.parse(html),
          {:ok, updated_bookmark} <- Bookmarks.update_bookmark(bookmark, %{title: result.title}) do
-      Tags.list_tags_with_rules()
-      |> Tags.Rules.parse(updated_bookmark)
-      |> Bookmarks.update_tags(updated_bookmark)
+      {:ok, updated_bookmark} =
+        Tags.list_tags_with_rules()
+        |> Tags.Rules.parse(updated_bookmark)
+        |> IO.inspect()
+        |> Bookmarks.update_tags(updated_bookmark)
 
       Phoenix.PubSub.broadcast(TomieWeb.PubSub, "Bookmarks.Worker:#{bookmark.id}", %{
         event: :updated,
