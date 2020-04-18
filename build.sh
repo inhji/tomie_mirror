3414ae779a6d7edc3bed3224f271ec22e8c736c8
Inhji <johnnie@posteo.de>
2020-04-18 09:06:45 +0200

    perf: run npm run deply with node env production

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ mix deps.get --only prod
 MIX_ENV=prod mix deps.compile
 
 npm install --prefix ./apps/tomie_web/assets
-npm run deploy --prefix ./apps/tomie_web/assets
+NODE_ENV=production npm run deploy --prefix ./apps/tomie_web/assets
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release --overwrite
