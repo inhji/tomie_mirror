70c520ee65a9b76f104347a37d52b6ea524add05
Inhji <johnnie@posteo.de>
2020-04-21 00:20:29 +0200

    improvement: nicer job overview

--- apps/tomie_web/lib/tomie_web/views/job_view.ex
+++ apps/tomie_web/lib/tomie_web/views/job_view.ex
@@ -1,3 +1,7 @@
 defmodule TomieWeb.JobView do
   use TomieWeb, :view
+
+  def job_duration(%{scheduled_at: scheduled_at, completed_at: completed_at} = job) do
+    Timex.diff(completed_at, scheduled_at, :milliseconds)
+  end
 end
