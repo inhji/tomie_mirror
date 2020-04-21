3cbb84fdcce7fb4650daaa75bacee79468c78e50
Inhji <johnnie@posteo.de>
2020-04-21 20:11:20 +0200

    improvement: add iconview to render svg icons

---
+++ apps/tomie_web/lib/tomie_web/views/icon_view.ex
@@ -0,0 +1,77 @@
+defmodule IconView do
+  use TomieWeb, :view
+
+  def icon(name), do: svg_icon(name)
+
+  defp svg_icon(:star) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:time) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-1-7.59V4h2v5.59l3.95 3.95-1.41 1.41L9 10.41z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:arrow_outline_up) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M10 0a10 10 0 1 1 0 20 10 10 0 0 1 0-20zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm2 8v5H8v-5H5l5-5 5 5h-3z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:inbox) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M0 2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm14 12h4V2H2v12h4c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:heart) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:box) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M0 2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v2H0V2zm1 3h18v13a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V5zm6 2v2h6V7H7z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:user) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:light_bulb) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M7 13.33a7 7 0 1 1 6 0V16H7v-2.67zM7 17h6v1.5c0 .83-.67 1.5-1.5 1.5h-3A1.5 1.5 0 0 1 7 18.5V17zm2-5.1V14h2v-2.1a5 5 0 1 0-2 0z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:home) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z"/>
+    </svg>
+    """
+  end
+end
