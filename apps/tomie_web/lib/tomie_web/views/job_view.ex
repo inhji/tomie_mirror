9013bd3af784b3b3a21638f1ae46d8118ee00af9
Inhji <johnnie@posteo.de>
2020-04-22 08:39:03 +0200

    improvement: show more job info on job show

--- apps/tomie_web/lib/tomie_web/views/job_view.ex
+++ apps/tomie_web/lib/tomie_web/views/job_view.ex
@@ -2,10 +2,13 @@ defmodule TomieWeb.JobView do
   use TomieWeb, :view
   alias TomieWeb.JobLive
 
-  def job_duration(%{scheduled_at: scheduled_at, completed_at: completed_at} = job) do
+  def job_duration(%{scheduled_at: scheduled_at, completed_at: completed_at} = _job) do
     Timex.diff(completed_at, scheduled_at, :milliseconds)
   end
 
   def job_time(nil), do: "empty"
   def job_time(datetime), do: Timex.from_now(datetime)
+
+  def format(nil), do: "empty"
+  def format(datetime), do: Timex.format!(datetime, "{h24}:{m}:{s}{ss}")
 end
