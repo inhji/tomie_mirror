15a875d6942dad14458ac6cfb277fb3b1463ac3a
Inhji <johnnie@posteo.de>
2020-04-24 09:35:49 +0200

    fix: duration crashes when completed_at is nil

--- apps/tomie_web/lib/tomie_web/views/job_view.ex
+++ apps/tomie_web/lib/tomie_web/views/job_view.ex
@@ -2,8 +2,12 @@ defmodule TomieWeb.JobView do
   use TomieWeb, :view
   alias TomieWeb.JobLive
 
+  def job_duration(%{scheduled_at: scheduled_at, completed_at: nil} = _job) do
+    "Running"
+  end
+
   def job_duration(%{scheduled_at: scheduled_at, completed_at: completed_at} = _job) do
-    Timex.diff(completed_at, scheduled_at, :milliseconds)
+    to_string(Timex.diff(completed_at, scheduled_at, :milliseconds)) <> "ms"
   end
 
   def job_time(nil), do: "empty"
