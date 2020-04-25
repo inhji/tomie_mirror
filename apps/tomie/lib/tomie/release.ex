0d9d589464ed736651a960884d00f5dc6c771891
Inhji <johnnie@posteo.de>
2020-04-26 00:41:52 +0200

    fix: migrator

--- apps/tomie/lib/tomie/release.ex
+++ apps/tomie/lib/tomie/release.ex
@@ -8,7 +8,7 @@ defmodule Tomie.Release do
   end
 
   def rollback(repo, version) do
-    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, step: 1))
   end
 
   defp repos do
