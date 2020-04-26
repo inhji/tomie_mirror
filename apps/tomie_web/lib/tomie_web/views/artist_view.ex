a062b020166892dde23fa0cc842f66c60713c92c
Inhji <johnnie@posteo.de>
2020-04-26 11:23:00 +0200

    feat: show last listens per album

--- apps/tomie_web/lib/tomie_web/views/artist_view.ex
+++ apps/tomie_web/lib/tomie_web/views/artist_view.ex
@@ -1,4 +1,14 @@
 defmodule TomieWeb.ArtistView do
   use TomieWeb, :view
   alias TomieWeb.ArtistLive
+
+  def last_listen(album) do
+    album.listens
+    |> Enum.sort(fn a, b ->
+      DateTime.compare(a.listened_at, b.listened_at) == :gt
+    end)
+    |> Enum.at(0)
+    |> Map.get(:listened_at)
+    |> Timex.from_now()
+  end
 end
