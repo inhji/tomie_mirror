a44873629422975cc1cc850759e3c99ff97576d6
Inhji <johnnie@posteo.de>
2020-05-02 12:30:52 +0200

    feat: add card footer, remote root note component

--- apps/tomie_web/lib/tomie_web/live/note_live/components/note_component.ex
+++ apps/tomie_web/lib/tomie_web/live/note_live/components/note_component.ex
@@ -1,13 +1,7 @@
 defmodule TomieWeb.NoteLive.NoteComponent do
   use TomieWeb, :live_component
 
-  def render(%{note: %{root: true} = _note} = assigns) do
-    TomieWeb.NoteView.render("root_component.html", assigns)
-  end
-
-  def render(assigns) do
-    TomieWeb.NoteView.render("note_component.html", assigns)
-  end
+  def render(assigns), do: TomieWeb.NoteView.render("note_component.html", assigns)
 
   def update(%{note: note}, socket) do
     {:ok, children} = Notes.Tree.descendants(note.id, nodes: true, depth: 1)
