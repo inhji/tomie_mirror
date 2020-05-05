0d453368a9f0f151d0826a9cd9a92e43ecbd26be
Inhji <johnnie@posteo.de>
2020-05-05 21:15:34 +0200

    feat: add icons to bookmark buttons

--- apps/tomie_web/lib/tomie_web/views/icon_view.ex
+++ apps/tomie_web/lib/tomie_web/views/icon_view.ex
@@ -12,6 +12,22 @@ defmodule TomieWeb.IconView do
     """
   end
 
+  defp svg_icon(:link) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M10 0l10 10-10 10L0 10 10 0zM6 10v3h2v-3h3v3l4-4-4-4v3H8a2 2 0 0 0-2 2z"/>
+    </svg>
+    """
+  end
+
+  defp svg_icon(:edit) do
+    ~E"""
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/>
+    </svg>
+    """
+  end
+
   defp svg_icon(:bookmark) do
     ~E"""
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
