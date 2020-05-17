03fdabd272bce38f83a153b0ca82d19f8e56ece7
Inhji <johnnie@posteo.de>
2020-05-17 22:45:23 +0200

    chore: remove credo, excoveralls

--- mix.exs
+++ mix.exs
@@ -46,8 +46,6 @@ defmodule Tomie.Umbrella.MixProject do
   # and cannot be accessed from applications inside the apps folder
   defp deps do
     [
-      {:excoveralls, "~> 0.12.3", only: :test},
-      {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:git_ops, "~> 2.0.0", only: [:dev, :test]}
     ]
