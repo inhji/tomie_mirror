23e9ddeb54c8929635353e2380d5a87869fd989b
Inhji <johnnie@posteo.de>
2020-05-24 22:06:26 +0200

    fix: unused vars

--- apps/tomie_web/lib/tomie_web/live/page_live/index.ex
+++ apps/tomie_web/lib/tomie_web/live/page_live/index.ex
@@ -4,7 +4,7 @@ defmodule TomieWeb.PageLive.Index do
 
   def render(assigns), do: PageView.render("index.html", assigns)
 
-  def mount(_params, session, socket) do
+  def mount(_params, _session, socket) do
     weather = Cachex.get!(:weather, :now)
     {:ok, socket |> assign(weather: weather)}
   end
