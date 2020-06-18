2ebb9abba852999c40b48d3fa5aa05309ca8e40b
Inhji <johnnie@posteo.de>
2020-06-18 20:49:51 +0200

    tweak: mix format

--- apps/tomie_web/lib/tomie_web/views/home_view.ex
+++ apps/tomie_web/lib/tomie_web/views/home_view.ex
@@ -2,8 +2,8 @@ defmodule TomieWeb.HomeView do
   use TomieWeb, :view
 
   def tag_string(tags) do
-  	tags
-  	|> Enum.map(fn (t) -> t.name end) 
-  	|> Enum.join(", ")
+    tags
+    |> Enum.map(fn t -> t.name end)
+    |> Enum.join(", ")
   end
 end
