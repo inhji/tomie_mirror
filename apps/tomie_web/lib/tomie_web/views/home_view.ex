a61388ffa63db6e3647df4130695f6fe40055860
Inhji <johnnie@posteo.de>
2020-06-21 11:10:25 +0200

    fix: handle empty bookmark tags

--- apps/tomie_web/lib/tomie_web/views/home_view.ex
+++ apps/tomie_web/lib/tomie_web/views/home_view.ex
@@ -2,8 +2,14 @@ defmodule TomieWeb.HomeView do
   use TomieWeb, :view
 
   def tag_string(tags) do
-    tags
-    |> Enum.map(fn t -> t.name end)
-    |> Enum.join(", ")
+    case tags do
+      [] ->
+        "No Tags"
+
+      _ ->
+        tags
+        |> Enum.map(fn t -> t.name end)
+        |> Enum.join(", ")
+    end
   end
 end
