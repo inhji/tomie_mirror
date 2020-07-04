baacc207cb813a40ff204fcbd20e97764816032f
Inhji <johnnie@posteo.de>
2020-07-04 09:50:50 +0200

    fix: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh
 
 ssh -T tomie@glados << EOSSH
+source ~/.zshrc
 cd ~/tomie
 git pull
 ./build.sh
+sudo systemctl restart tomie
 exit
 EOSSH
-#ssh -T glados systemctl restart tomie
