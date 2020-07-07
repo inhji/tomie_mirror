9f39a34eca51893cdede4dde2f04cbfb1a571874
Inhji <johnnie@posteo.de>
2020-07-07 09:04:48 +0200

    fix: improve deploy script to actually restart service after deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env zsh
 
+echo "Build starting!"
+echo "--------------------------"
 ssh -T tomie@glados << EOSSH
 source ~/.zshrc
 cd ~/tomie
 git pull
 ./build.sh
-sudo systemctl restart tomie
-exit
 EOSSH
+echo "Build complete, restarting..."
+ssh -T tomie@glados sudo systemctl restart tomie
